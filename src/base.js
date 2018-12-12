79b2e71d12d835198e53f076b5c17eb48ed01946
Axl Wang <axlwang1976@gmail.com>
2018-12-12 09:30:40 +0800

    finish

--- src/base.js
+++ src/base.js
@@ -2,7 +2,7 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyDFUC9SMq7GGA3GNhbWe-9q28I8Vj-G-tI',
+  apiKey: 'xxx',
   authDomain: 'personalsitereact.firebaseapp.com',
   databaseURL: 'https://personalsitereact.firebaseio.com'
 });
